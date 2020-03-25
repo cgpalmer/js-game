639f3d331bcd6ae9e65f6486a6a071f87156cb33
cgpalmer <cgpalmer91@gmail.com>
2020-03-25 00:04:37 +0000

    Added in function to display the pokemon encounter when one of the boxes are clicked.

--- js/main.js
+++ js/main.js
@@ -8,3 +8,9 @@ $("#player2").click(function(){
     $("#gameboard2").css("display", "inline-block");
     $("#gameboard1").css("display", "none");
 });
+
+$(".box").click(function(){
+    $("#displayEncounter").css("display", "block");
+
+});
+
